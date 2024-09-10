5bc8def96875d238c78a88edfc3d7a4bec23c9a7
cherishegoyibo <cherishegoyibo@gmail.com>
2024-09-10 18:09:23 +0100

    feat: created services functions for handling registration and login.

---
+++ services.py
@@ -0,0 +1,26 @@
+from model import UserModel
+from extensions import db
+from sqlalchemy.exc import IntegrityError
+
+class Auth:
+    def user_register(name, email, password, pnumber):
+        user = UserModel(name=name, email=email, pnumber=pnumber)
+        user.set_password(password=password)
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+
+            return user.email, 200
+        
+        except IntegrityError as e:
+            return e, 400
+        
+    def login(email, pswd):
+        user = UserModel.query.filter(UserModel.email==email).one()
+        if user and user.check_password(pswd):
+            return 'Success', 200
+        
+        return 'Error', 401
+
+
