b3e4ba7787208c4979b028cc094923423400ff4a
cherishegoyibo <cherishegoyibo@gmail.com>
2024-09-12 14:26:23 +0100

    Chore: restructuring

---
+++ backend/app/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+# from app.services import Auth
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "YouDontGetTheKey"
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:////app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        pswd = request.form.get('password')
+
+        msg, status = Auth.login(email, pswd)
+
+        if status == 200:
+            flash(message=msg)
+            return redirect(url_for('index'))
+        
+    return render_template('login.html')
+
+
+def index():
+    return render_template('index.html')
+
+
+@app.route('/register')
+def customer_register():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        pnumber = request.form.get('pnumber')
+        password = request.form.get('password')
+
+        message, status = Auth.user_register(
+            name=name, email=email, password=password,
+            pnumber=pnumber
+            )
+        
+        if status == 200:
+            flash(message=f'{message} successfully registered.')
+            return render_template('login.html')
+        
+        elif status == 400:
+            flash(message='error')
+        
+        
+    return render_template('registration.html')
+    
+
+
+
+app.run()
+
---
+++ backend/app/services.py
@@ -0,0 +1,26 @@
+from app.model import UserModel
+from app.extensions import db
+from sqlalchemy.exc import IntegrityError
+
+class Auth:
+    def user_register(name, email, password, pnumber):
+        user = UserModel(name=name, email=email, pnumber=pnumber)
+        user.set_password(password=password)
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+
+            return user.email, 200
+        
+        except IntegrityError as e:
+            return e, 400
+        
+    def login(email, pswd):
+        user = UserModel.query.filter_by(email=email).first()
+        if (user is not None) and user.check_password(pswd):
+            return 'Success', 200
+        
+        return 'Error', 401
+
+
